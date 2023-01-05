c0ce40c38093f84261a0267ce59a68a2e0fdada9
thom-OrigamiLabs <121872729+thom-OrigamiLabs@users.noreply.github.com>
2023-01-05 13:56:23 +0000

    Added opencv framereader class
    Mostly copied from ffmpeg frame reader, needs improving

--- stonesoup/reader/video.py
+++ stonesoup/reader/video.py
@@ -7,13 +7,14 @@ of video data/streams.
 import datetime
 import threading
 from queue import Queue
-from typing import Mapping, Tuple, Sequence, Any
+from typing import Mapping, Tuple, Sequence, Any, List
 from urllib.parse import ParseResult
 
 import numpy as np
 try:
     import ffmpeg
     import moviepy.editor as mpy
+    import cv2
 except ImportError as error:
     raise ImportError(
         "Usage of video processing classes requires that the optional"
@@ -208,3 +209,223 @@ class FFmpegVideoStreamReader(UrlReader, FrameReader):
 
                 # Write new frame to buffer
                 self.buffer.put(frame)
+
+    def terminate(self):
+        self.stream.terminate()
+
+
+class FreshestFrame(threading.Thread):
+    def __init__(self, capture, name='FreshestFrame'):
+        self.capture = capture
+        assert self.capture.isOpened()
+        
+
+        # this lets the read() method block until there's a new frame
+        self.cond = threading.Condition()
+
+        # this allows us to stop the thread gracefully
+        self.running = False
+
+        # keeping the newest frame around
+        self.frame = None
+
+        # passing a sequence number allows read() to NOT block
+        # if the currently available one is exactly the one you ask for
+        self.latestnum = 0
+
+        # this is just for demo purposes        
+        self.callback = None
+        
+        super().__init__(name=name)
+        self.start()
+
+    def start(self):
+        self.running = True
+        super().start()
+
+    def release(self, timeout=None):
+        self.running = False
+        self.join(timeout=timeout)
+        self.capture.release()
+        
+    def isOpened(self):
+    	return self.capture.isOpened()
+
+    def run(self):
+        counter = 0
+        while self.running:
+            # block for fresh frame
+            (rv, img) = self.capture.read()
+            assert rv
+            counter += 1
+
+            # publish the frame
+            with self.cond: # lock the condition for this operation
+                self.frame = img if rv else None
+                self.latestnum = counter
+                self.cond.notify_all()
+
+            if self.callback:
+                self.callback(img)
+
+    def read(self, wait=True, seqnumber=None, timeout=None):
+        # with no arguments (wait=True), it always blocks for a fresh frame
+        # with wait=False it returns the current frame immediately (polling)
+        # with a seqnumber, it blocks until that frame is available (or no wait at all)
+        # with timeout argument, may return an earlier frame;
+        #   may even be (0,None) if nothing received yet
+
+        with self.cond:
+            if wait:
+                if seqnumber is None:
+                    seqnumber = self.latestnum+1
+                if seqnumber < 1:
+                    seqnumber = 1
+                
+                rv = self.cond.wait_for(lambda: self.latestnum >= seqnumber, timeout=timeout)
+                if not rv:
+                    return (self.latestnum, self.frame)
+
+            return (self.latestnum, self.frame)
+
+
+class OpenCVVideoStreamReader(FrameReader):
+    """ FFmpegVideoStreamReader
+
+    A threaded reader that uses ffmpeg-python_ to read frames from video
+    streams (e.g. RTSP) in real-time.
+
+
+    Notes
+    -----
+    - Use of this class requires that FFmpeg_ is installed on the host machine.
+    - By default, FFmpeg performs internal buffering of frames leading to a \
+    slight delay in the incoming frames (0.5-1 sec). To remove the delay it \
+    is recommended to set ``input_opts={'threads': 1, 'fflags': 'nobuffer'}`` \
+    when instantiating a reader, e.g: .
+
+    .. code-block:: python
+
+        video_reader = FFmpegVideoStreamReader('rtsp://192.168.0.10:554/1/h264minor',
+                                               input_opts={'threads': 1, 'fflags': 'nobuffer'})
+        for timestamp, frame in video_reader:
+            ....
+
+    .. _ffmpeg-python: https://github.com/kkroening/ffmpeg-python
+    .. _FFmpeg: https://www.ffmpeg.org/download.html
+
+    """
+
+
+    videosource: str = Property(doc="A video file to read")
+    buffer_size: int = Property(
+        default=1,
+        doc="Size of the frame buffer. The frame buffer is used to cache frames in cases where "
+            "the stream generates frames faster than they are ingested by the reader. If "
+            "`buffer_size` is less than or equal to zero, the buffer size is infinite.")
+    input_opts: List = Property(
+        default=None,
+        doc="FFmpeg input options, provided in the form of a dictionary, whose keys correspond to "
+            "option names. (e.g. ``{'fflags': 'nobuffer'}``). The default is ``{}``.")
+    output_opts: Mapping[str, str] = Property(
+        default=None,
+        doc="FFmpeg output options, provided in the form of a dictionary, whose keys correspond "
+            "to option names. The default is ``{'f': 'rawvideo', 'pix_fmt': 'rgb24'}``.")
+    filters: Sequence[Tuple[str, Sequence[Any], Mapping[Any, Any]]] = Property(
+        default=None,
+        doc="FFmpeg filters, provided in the form of a list of filter name, sequence of "
+            "arguments, mapping of key/value pairs (e.g. ``[('scale', ('320', '240'), {})]``). "
+            "Default `None` where no filter will be applied. Note that :attr:`frame_size` may "
+            "need to be set in when video size changed by filter.")
+    frame_size: Tuple[int, int] = Property(
+        default=None,
+        doc="Tuple of frame width and height. Default `None` where it will be detected using "
+            "`ffprobe` against the input, but this may yield wrong width/height (e.g. when "
+            "filters are applied), and such this option can be used to override.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.input_opts is None:
+            self.input_opts = []
+        if self.output_opts is None:
+            self.output_opts = {'f': 'rawvideo', 'pix_fmt': 'rgb24', 'colourTransform': cv2.COLOR_BGR2RGB}
+        if self.filters is None:
+            self.filters = []
+
+        self.buffer = Queue(maxsize=self.buffer_size)
+
+        
+        # else:
+        #     # Probe stream information
+        #     self._stream_info = next(
+        #         s
+        #         for s in ffmpeg.probe(self.url.geturl(), **self.input_opts)['streams']
+        #         if s['codec_type'] == 'video')
+
+        # Initialise stream
+        self.stream = FreshestFrame(cv2.VideoCapture(self.videosource, *self.input_opts))
+        # for filter_ in self.filters:
+        #     filter_name, filter_args, filter_kwargs = filter_
+        #     self.stream = self.stream.filter(
+        #         filter_name, *filter_args, **filter_kwargs
+        #     )
+        # self.stream = (
+        #     self.stream
+        #     .output('pipe:', **self.output_opts)
+        #     .global_args('-y', '-loglevel', 'panic')
+        #     .run_async(pipe_stdout=True)
+        # )
+
+        if self.frame_size is not None:
+            self._stream_info = {
+                'width': self.frame_size[0],
+                'height': self.frame_size[1]}
+        #else:
+        #    self._stream_info = {
+        #        'width': self.stream.get(cv2.CAP_PROP_FRAME_WIDTH),
+        #        'height': self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)}
+
+        # Initialise capture thread
+        self._capture_thread = threading.Thread(target=self._run)
+        self._capture_thread.daemon = True
+        self._capture_thread.start()
+
+    @BufferedGenerator.generator_method
+    def frames_gen(self):
+        while self._capture_thread.is_alive():
+            # if not self.buffer.empty():
+            frame = self.buffer.get()
+            timestamp = frame.timestamp
+            yield timestamp, frame
+
+    def _run(self):
+        while self.stream.isOpened():
+            #width = int(self._stream_info['width'])
+            #height = int(self._stream_info['height'])
+
+            ret, frame = self.stream.read()
+
+            if not ret:
+                break
+            else:
+                if 'colourTransform' in self.output_opts:
+                    frame_np = cv2.cvtColor(frame, self.output_opts['colourTransform'])
+                    frame = ImageFrame(frame_np, datetime.datetime.now())
+                    self.buffer.put(frame)
+
+            # Read bytes from stream
+            # in_bytes = self.stream.stdout.read(width * height * 3)
+
+            # if in_bytes:
+            #     # Transform bytes to pixels
+            #     frame_np = (
+            #         np.frombuffer(in_bytes, np.uint8)
+            #         .reshape([height, width, 3])
+            #     )
+            #     frame = ImageFrame(frame_np, datetime.datetime.now())
+
+            #     # Write new frame to buffer
+            #     self.buffer.put(frame)
+
+    def terminate(self):
+        self.stream.release()
